c10267947111386020b87536ab7467ebb9c5b2e2
EnzoRoerto <thrash.men@hotmail.com>
2017-07-22 00:11:21 -0400

    MIGRACIONES Y MODELOS

--- sitio-uno/app/User.php
+++ sitio-uno/app/User.php
@@ -8,14 +8,14 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
-
+    protected $table ='user';
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'rut', 'password', 'nickname',
     ];
 
     /**
@@ -26,4 +26,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function news(){
+        return $this ->hasMany('App\News');
+    }
 }
