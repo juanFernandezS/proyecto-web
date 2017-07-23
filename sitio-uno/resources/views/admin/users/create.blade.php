55e1bffc00a01cf37dccdb1a9c170611b6628308
juane <juan.e.fernandez.suazo@gmail.com>
2017-07-23 12:12:08 -0400

    arreglo controlador user

---
+++ sitio-uno/resources/views/admin/users/create.blade.php
@@ -0,0 +1,28 @@
+@extends('admin.template.main')
+
+@section('title', 'Crear Usuario')
+
+@section('content')
+    {!! Form::open(['route' => 'users.store', 'method' => 'POST']) !!}
+
+        <div class="form-group">
+            {!! Form::label('rut', 'Rut') !!}
+            {!! Form::text('rut', null, ['class' => 'form-control', 'placeholder' => '12.345.678-9', 'required']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('nickname', 'Pseudonimo') !!}
+            {!! Form::text('nickname', null, ['class' => 'form-control', 'placeholder' => '', 'required']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('password', 'Contraseña') !!}
+            {!! Form::password('password', ['class' => 'form-control', 'placeholder' => '************', 'required']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::submit('Registrar', ['class' => 'btn btn-primary']) !!}
+        </div>
+
+    {!! Form::close() !!}
+@endsection
